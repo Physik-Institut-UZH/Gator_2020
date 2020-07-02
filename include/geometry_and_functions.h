4b07a75e88431b91ff6d56051fc298b44075749f
Gabriela Rodrigues Araujo <grodriguesaraujo@protonmail.com>
2020-07-02 13:55:25 +0200

    include folder with easygator functions

---
+++ include/geometry_and_functions.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void check_matlist(string sim_dir);
+int build_geo(string samplename,string sim_dir);
+bool itExists(string dirName_in);
+string checkfunction(string data_dir, string samplefilename);
+void printsummary(string s_dir);
