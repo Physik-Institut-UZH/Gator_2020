6432bf5e9f74b111f65b98545d54b69a06138d47
Gabriela Rodrigues Araujo <grodriguesaraujo@protonmail.com>
2021-12-14 16:53:34 +0100

    removed file by mistake

---
+++ analysis/isotopes.cpp
@@ -0,0 +1,17 @@
+#include "isotopes.h"
+bool i_238U = true;
+bool i_232Th = true;
+bool i_40K = true;
+bool i_60Co = true;
+bool i_137Cs = true;
+bool i_226Ra = true;
+bool i_235U = true;
+bool i_54Mn = true;
+bool i_58Co = true;
+bool i_56Co = false;
+bool i_46Sc = false;
+bool i_186keV = false;
+bool i_1001keV = false;
+bool i_Pa = false;
+bool i_7Be = false;
+bool i_210Pb = true;
